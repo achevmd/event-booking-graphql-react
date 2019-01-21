fe06a2ccf1ed4800cc56dc6314c1357ff1cab909
Martin Achev <achevmd@gmail.com>
2019-01-21 10:01:56 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolvers = require('./graphql/resolvers');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+  schema: graphqlSchema,
+  rootValue: graphqlResolvers,
+  graphiql: true
+}));
+
+const connectionStr = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-crqqi.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`;
+mongoose.connect(connectionStr, {
+  useNewUrlParser: true
+})
+  .then(() => {
+    console.log('Connected to database');
+    app.listen(3000, () => console.log('Server running: 3000'));
+  })
+  .catch(err => console.log(err));
