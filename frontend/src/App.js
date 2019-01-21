2ff99c489ed3ba0ddf199555d1716a257ec080e9
Martin Achev <achevmd@gmail.com>
2019-01-21 14:54:45 +0200

    Implemented ReactJS

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth'
+import EventsPage from './pages/Events';
+import BookingsPage from './pages/Bookings';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Redirect path="/" to="/auth" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventsPage} />
+          <Route path="/bookings" component={BookingsPage} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
